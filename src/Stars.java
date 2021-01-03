6e6097b0ae99a29b25824b862bca8559e81ad25a
Radoslaw Choluj <radekcholuj@wp.pl>
2021-01-03 23:28:25 +0100

    #5 wyswietlenie gwiazdek dla n=4 i n=6

---
+++ src/Stars.java
@@ -0,0 +1,27 @@
+public class Stars {
+    public static void main(String[] args) {
+
+
+        int count = 0;
+        int starNumber = 4;
+        String star = "";
+
+        while (count < starNumber) {
+            star += "*";
+            System.out.println(star);
+            count++;
+        }
+
+        starNumber = 6;
+        count = 0;
+        star = "";
+        System.out.println();
+
+        while (count < starNumber) {
+            star += "*";
+            System.out.println(star);
+            count++;
+        }
+
+    }
+}
